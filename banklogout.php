07e68a36a464ab3d00fae649f77b5175110fb2c8
Alex Sylvain Luenga <38809975+0788611515@users.noreply.github.com>
2022-05-28 13:21:16 +0200

    Update banklogout.php

--- banklogout.php
+++ banklogout.php
@@ -3,6 +3,7 @@ session_start();
 unset($_SESSION['id']);
 unset($_SESSION['fullname']);
 ?>
+
 <script language="javascript">
 	alert("System Log In Session Successfully Ended..Thank You");
 window.location="banklogin.php";
